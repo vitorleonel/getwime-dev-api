8cf8e6b573cafe8181c51cced18ffd61de6589c6
Vitor Leonel <falecom@vitorleonel.com>
2020-04-11 23:59:49 -0300

    implement signin

--- app/Http/Controllers/Auth/SignInController.php
+++ app/Http/Controllers/Auth/SignInController.php
@@ -3,12 +3,53 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\SignInRequest;
 
+use App\Models\User;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class SignInController
+ * @package App\Http\Controllers\Auth
+ */
 class SignInController extends Controller
 {
-    public function __invoke()
+
+    /**
+     * @param SignInRequest $request
+     * @return JsonResponse
+     */
+    public function __invoke(SignInRequest $request)
     {
-        // TODO: Implement __invoke() method.
+        try {
+            $user = User::where('key_access', $request->key_access)->first();
+
+            if(!$user) {
+                throw new Exception('Chave de acesso inválida.', 404);
+            }
+
+            $user->tokens()->delete();
+            $token = $this->generateToken($user);
+
+            return response()->json([
+                'token' => $token,
+                'user' => $user
+            ]);
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], $exception->getCode());
+        }
+    }
+
+    /**
+     * @param User $user
+     * @return string
+     */
+    private function generateToken(User $user)
+    {
+        $name = request()->device_name || 'Default';
+
+        return $user->createToken($name)->plainTextToken;
     }
 }
