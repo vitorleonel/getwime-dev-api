659d213859284f0ab1a6dd4df519910d5fb6f641
Vitor Leonel <falecom@vitorleonel.com>
2020-04-12 00:41:16 -0300

    add chat model

---
+++ database/migrations/2020_04_12_031638_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('public_id', 36)->index();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
